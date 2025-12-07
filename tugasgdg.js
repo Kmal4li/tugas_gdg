b8e3262ed81a6bdb8396825ac95f40a1c7b9aa8d
Kmal4li <m4kmalali@gmail.com>
2025-12-07 16:19:25 +0700

    feat: age logic

--- tugasgdg.js
+++ tugasgdg.js
@@ -1,3 +1,22 @@
 const readline = require("readline");
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question("Masukkan nama: ", (name) => {
+  rl.question("Masukkan umur: ", (ageInput) => {
+    const age = Number(ageInput);
+
+    if (age < 21) {
+      console.log(`Maaf ${name}, umur kamu kurang ${21 - age} tahun lagi`);
+      rl.close();
+      return;
+    }
+
+    console.log(`Selamat ${name}, kamu sudah cukup umur`);
+    rl.close();
+  });
+});
 
